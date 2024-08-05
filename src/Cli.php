320a8053344239d4be82c1ffdc20fb38ae670b06
Biryukov Roman <roman.br88@yandex.ru>
2024-08-05 16:25:39 +0300

    fix ending

--- src/Cli.php
+++ src/Cli.php
@@ -9,7 +9,7 @@ use function cli\prompt;
 
 function greet(): void
 {
-    line('Welcome to the Brain Game!');
+    line('Welcome to the Brain Games!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
 }
